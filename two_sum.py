ee485265187239c6683a099f59d4fd959d0750da
Oliver Boamah <oliverboamah@yahoo.com>
2020-09-10 20:28:23 +0000

    Create two_sum.py

---
+++ two_sum.py
@@ -0,0 +1,23 @@
+''''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+'''
+# solution class
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        dictionary = {}
+        for i in range(len(nums)):
+            dictionary.update({nums[i]: i})
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in dictionary and dictionary[complement] != i:
+                return [i, dictionary[complement]]
+
+# example            
+result = Solution.twoSum([2, 7, 11, 15])
+print(result)
