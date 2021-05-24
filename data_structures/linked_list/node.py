8075be2550b571c9b7002dfaae16fc2ad3f86b65
Oliver Boamah <oliverboamah@yahoo.com>
2021-05-24 08:28:36 +0000

    Add node class, Add .gitignore file.

---
+++ data_structures/linked_list/node.py
@@ -0,0 +1,12 @@
+class Node:
+    """
+    This class represents an element(node) in a linked list.
+
+    Attributes:
+        data: The value of the node
+        next: The reference to the next node in the linked list
+    """
+
+    def __init__(self):
+        self.data = None
+        self.next = None
